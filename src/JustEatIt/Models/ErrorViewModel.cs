74cd80f251f9437b4b0a9ed7e4eabb4fe32ff0e8
BullFrog13 <13heavyweather@gmail.com>
2019-10-12 13:08:22 -0400

    Project init commit

---
+++ src/JustEatIt/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JustEatIt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
