63fde6c98b6c5b5df5e791efb0e0c9c526b04576
Jovane Marques <jovanemarques@gmail.com>
2019-11-01 22:05:25 -0400

    Removing fixed data. Saving all data when updating.

--- src/JustEatIt/Data/Entities/EFDishRepository.cs
+++ src/JustEatIt/Data/Entities/EFDishRepository.cs
@@ -16,19 +16,7 @@ namespace JustEatIt.Data.Entities
             this.context = context;
         }
 
-        //public IQueryable<Dish> GetAll => context.Dish;
-        public IQueryable<Dish> GetAll => new List<Dish> { 
-                    new Dish {Id = 1, Name = "Pepperoni Pizza", Description = "Delicious pizza", Price = 5.99f, 
-                        BestBefore = new DateTime(2019, 12, 25), 
-                        Image = "https://img.buzzfeed.com/thumbnailer-prod-us-east-1/" +
-                        "dc23cd051d2249a5903d25faf8eeee4c/BFV36537_CC2017_2IngredintDough4Ways-FB.jpg",
-                        Type = "Pizza", Restaurant = "Italian", Quantity = 50
-                    },
-                    new Dish {Id = 2, Name = "Cheese Pizza", Description = "Delicious Pizza", Price = 5.99f, BestBefore = new DateTime(2019, 12, 26),
-                        Image = "https://www.countrysidecravings.com/wp-content/uploads/2017/03/Three-Cheese-Pizza-2-500x375.jpg",
-                        Type = "Pizza", Restaurant = "Italian", Quantity = 25
-                    }
-                }.AsQueryable<Dish>();
+        public IQueryable<Dish> GetAll => context.Dish;
 
         public Dish Save(Dish dish)
         {
@@ -41,8 +29,14 @@ namespace JustEatIt.Data.Entities
                 Dish dbDish = context.Dish.FirstOrDefault(r => r.Id == dish.Id);
                 if (dbDish != null)
                 {
+                    dbDish.Name = dish.Name;
                     dbDish.Description = dish.Description;
-                    dbDish.Partner = dish.Partner;
+                    dbDish.Price = dish.Price;
+                    dbDish.Quantity = dish.Quantity;
+                    dbDish.BestBefore = dish.BestBefore;
+                    dbDish.Image = dish.Image;
+                    dbDish.Type = dish.Type;
+                    dbDish.Restaurant = dish.Restaurant;
                 }
             }
             context.SaveChanges();
