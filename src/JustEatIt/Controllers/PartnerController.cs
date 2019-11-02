c0a2bea4e8371417dfee9c4c358df1b411faa87c
dl-ac <44243088+dl-ac@users.noreply.github.com>
2019-11-02 12:28:32 -0400

    Partner screen

--- src/JustEatIt/Controllers/PartnerController.cs
+++ src/JustEatIt/Controllers/PartnerController.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Security.Claims;
 using JustEatIt.Data.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JustEatIt.Controllers
 {
+    [Authorize]
     public class PartnerController : Controller
     {
         IDishRepository dishRepo;
@@ -18,8 +18,8 @@ namespace JustEatIt.Controllers
 
         public IActionResult Index()
         {
-            User.
-            return View();
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return View(dishRepo.GetAll.Where(d => d.Partner.Id == userId));
         }
     }
 }
